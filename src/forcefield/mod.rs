f6a11dd15088665903046d127080e30d49a83efa
Jose <jigonzalez930209@gmail.com>
2026-03-06 13:59:26 -0300

    feat(forcefield): update forcefield module definitions

--- src/forcefield/mod.rs
+++ src/forcefield/mod.rs
@@ -1,9 +1,13 @@
+pub mod bounds_ff;
 pub mod energy;
+pub mod etkdg_lite;
 pub mod gradients;
 pub mod minimizer;
-pub mod bounds_ff;
-pub mod etkdg_lite;
+pub mod mmff94;
+pub mod traits;
+pub mod uff;
 
 pub use energy::*;
 pub use gradients::*;
 pub use minimizer::*;
+pub use traits::*;
