805a96ef33ddbe724fc097b4ccc44dc3a43dee09
Jose <jigonzalez930209@gmail.com>
2026-03-06 16:26:21 -0300

    refactor(distgeom): export new ETKDG distance selection

--- src/distgeom/mod.rs
+++ src/distgeom/mod.rs
@@ -6,4 +6,5 @@ pub use bounds::{calculate_bounds_matrix, get_bond_length, smooth_bounds_matrix,
 pub use chirality::{calc_chiral_volume, identify_chiral_sets};
 pub use embedding::{
     compute_metric_matrix, generate_3d_coordinates, generate_nd_coordinates, pick_random_distances,
+    pick_etkdg_distances,
 };
