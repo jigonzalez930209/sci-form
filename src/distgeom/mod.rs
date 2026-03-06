32643dfa484eace4cbf4679c1900be02bed9b1ac
Jose <jigonzalez930209@gmail.com>
2026-03-06 15:39:21 -0300

    refactor(distgeom): modularize distance geometry implementation

---
+++ src/distgeom/mod.rs
@@ -0,0 +1,9 @@
+pub mod bounds;
+pub mod chirality;
+pub mod embedding;
+
+pub use bounds::{calculate_bounds_matrix, get_bond_length, smooth_bounds_matrix, triangle_smooth};
+pub use chirality::{calc_chiral_volume, identify_chiral_sets};
+pub use embedding::{
+    compute_metric_matrix, generate_3d_coordinates, generate_nd_coordinates, pick_random_distances,
+};
