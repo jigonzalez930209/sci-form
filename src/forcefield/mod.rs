e88e4b180f7f5a5776816219001cb5ed2029cba8
Jose <jigonzalez930209@gmail.com>
2026-03-06 15:39:51 -0300

    refactor(forcefield): update module exports for new robust implementation

--- src/forcefield/mod.rs
+++ src/forcefield/mod.rs
@@ -1,12 +1,15 @@
 pub mod bounds_ff;
+pub mod dg_terms;
 pub mod energy;
 pub mod etkdg_lite;
 pub mod gradients;
 pub mod minimizer;
 pub mod mmff94;
+pub mod params;
 pub mod traits;
 pub mod uff;
 
+pub use dg_terms::*;
 pub use energy::*;
 pub use gradients::*;
 pub use minimizer::*;
