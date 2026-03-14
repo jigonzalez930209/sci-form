07a7efedd4f1887c4d8322cc1853f506e13a8b05
Jose Ivan <jigonzalez930209@gmail.com>
2026-03-14 18:19:29 -0300

    style: apply rustfmt to src/alignment/mod.rs

--- src/alignment/mod.rs
+++ src/alignment/mod.rs
@@ -1,4 +1,4 @@
 //! Molecular alignment and RMSD calculation.
 
 pub mod kabsch;
-pub use kabsch::{compute_rmsd, align_coordinates, AlignmentResult};
+pub use kabsch::{align_coordinates, compute_rmsd, AlignmentResult};
