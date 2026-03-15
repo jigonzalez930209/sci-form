5818b3aa4be94b50b7b68a48e8d2d447d8004139
Jose Ivan <jigonzalez930209@gmail.com>
2026-03-15 16:20:29 -0300

    feat: implement parallel computation for total and projected DOS

--- src/dos/mod.rs
+++ src/dos/mod.rs
@@ -3,3 +3,5 @@
 #[allow(clippy::module_inception)]
 pub mod dos;
 pub use dos::{compute_dos, compute_pdos, DosResult};
+#[cfg(feature = "parallel")]
+pub use dos::{compute_dos_parallel, compute_pdos_parallel};
