b2d8362a75581f4eb061f5dcf05340bda29b0943
Jose Ivan <jigonzalez930209@gmail.com>
2026-03-22 17:57:02 -0300

    feat(nmr): add parallel ensemble-averaged J-couplings function

--- src/nmr/mod.rs
+++ src/nmr/mod.rs
@@ -11,6 +11,8 @@ pub mod hose;
 pub mod shifts;
 pub mod spectrum;
 
+#[cfg(feature = "parallel")]
+pub use coupling::ensemble_averaged_j_couplings_parallel;
 pub use coupling::{ensemble_averaged_j_couplings, predict_j_couplings, JCoupling, KarplusParams};
 pub use hose::{HoseCode, HoseShiftLookup};
 pub use shifts::{predict_chemical_shifts, ChemicalShift, NmrShiftResult};
